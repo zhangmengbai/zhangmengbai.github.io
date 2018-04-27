01a44808e3b78643276e8590c3d983f4857ed202
zhangmengbai <lixm@dliangzao.com>
2018-04-27 15:08:33 +0800

    mvvm1.2

---
+++ IFEMvvm1.2/src/main.js
@@ -0,0 +1,14 @@
+import san from "san"
+
+var MyApp = san.defineComponent({
+  template: '<p>Hello {{name}}!</p>',
+  
+  initData: function () {
+    return {
+      name: 'San'
+    };
+  }
+});
+
+var myApp = new MyApp();
+myApp.attach(document.body);
